fe1b2a2d7ff55d830f063975ac69d85488de2804
joshcoralvz <joshuacornelioalvarez@gmail.com>
2026-06-10 20:48:14 -0400

    Add files via upload

---
+++ mainAct2.py
@@ -0,0 +1,41 @@
+#Actividad 2: Manipulación de listas, tuplas y diccionarios
+carreras: tuple[str, ...] = ("Ingeniería de Software", "Contabilidad", "Derecho")
+
+
+personas: list[tuple[str, str, int, int]] = [
+    # nombre, apellido, edad, carrera
+]
+ 
+estudiantes: list[dict[str, str | int]] = [
+    # Lista de diccionario con nombre, apellido, edad, carrera
+]
+for _ in range(5):
+    # Solicita los datos aquí
+    nombre = input("Ingrese su nombre :")
+    apellido = input("Ingrese su apellido :")
+    edad = int(input("Ingrese su edad :"))
+    carrera = int(input("Ingresa su carrera (ingrese: 0 = Ingeniería de Software, 1 = Contabilidad, 2 = Derecho):"))
+
+    #creacion de tupla
+    datos = (nombre, apellido, edad, carrera)
+    personas.append(datos)#tupla agregada a la lista de personas
+
+    #recorrido de la lista personas
+    for datos in personas:
+        nombre = datos[0]
+        apellido = datos[1]
+        edad = datos[2]
+        carrera = carreras[datos[3]]
+
+        diccionario = {"nombre": nombre, "apellido": apellido, "edad": edad, "carrera": carrera}
+        estudiantes.append(diccionario)
+
+        for estudiante in estudiantes:
+            nombre = estudiante["nombre"]
+            apellido = estudiante["apellido"]
+            edad = estudiante["edad"]
+            carrera = estudiante["carrera"]
+        print("")
+        print(nombre, apellido, "tiene", edad, " años y estudia", carrera)
+        print("")
+
